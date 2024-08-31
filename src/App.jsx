537daf7bdff8b448cb98b2dc4cb9f473393db818
atul jaiswal <jaiswalatul018@gmai.com>
2024-08-31 18:12:41 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Hero from "./components/Hero";
+import Nav from "./components/Nav";
+const App=()=>{
+  return (
+    <div>
+      <Nav/>
+      <Hero/>
+    </div>
+  )
+};
+
+export default App;
